8810a3a55f78888f7d4a6988784a2dd701f09619
dej-27 <virus-ua@bk.ru>
2011-05-21 13:43:36 +0200

    storage helper

---
+++ Picaser/Helpers/StorageHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Xml;
+
+namespace Picaser.Helpers
+{
+    public class StorageHelper
+    {
+        public StorageHelper()
+        {
+            using (var appStorage = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (appStorage.FileExists("test.xml"))
+                {
+
+                    using (var file = appStorage.OpenFile("test.xml", FileMode.OpenOrCreate))
+                    {
+                        var reader = XmlReader.Create(file);
+                    }
+                }
+            }
+        }
+
+
+
+
+    }
+}
